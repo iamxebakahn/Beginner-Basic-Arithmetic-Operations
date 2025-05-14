20b36b70adabf507d8c37e7c76a8a1c5850f31f8
iamxebakahn <zebadwight@gmail.com>
2025-05-14 08:50:05 +0530

    Update Q2_Beginner_Basic_Arithmetic_Operations.java

--- Q2_Beginner_Basic_Arithmetic_Operations.java
+++ Q2_Beginner_Basic_Arithmetic_Operations.java
@@ -1,3 +1,16 @@
+// Problem Description
+// Print the following text in the output:
+
+// Hello
+
+// World !
+// Note: There is a empty line you need print between Hello and World !
+
+
+
+// Output Format
+
+// Print the required text in the output.
 import java.lang.*;
 import java.util.*;
 
