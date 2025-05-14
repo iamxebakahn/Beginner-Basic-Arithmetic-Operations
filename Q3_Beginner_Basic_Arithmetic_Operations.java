d177dc3838e081391fcb2a8c61e51feabeeae048
iamxebakahn <zebadwight@gmail.com>
2025-05-14 08:53:56 +0530

    Update Q3_Beginner_Basic_Arithmetic_Operations.java

--- Q3_Beginner_Basic_Arithmetic_Operations.java
+++ Q3_Beginner_Basic_Arithmetic_Operations.java
@@ -1,3 +1,20 @@
+// Problem Description
+// Print the following text:
+
+
+// Hello 40
+// 50 World !
+
+
+// Output Format
+
+// Print the output specifed in the problem description.
+
+
+// Example Output
+
+// Hello 40
+// 50 World !
 import java.lang.*;
 import java.util.*;
 
