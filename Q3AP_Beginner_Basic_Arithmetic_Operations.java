a45f096206dc924a9ccaa606e7e0ea6dd167f323
iamxebakahn <zebadwight@gmail.com>
2025-05-14 09:10:03 +0530

    Update Q3AP_Beginner_Basic_Arithmetic_Operations.java

--- Q3AP_Beginner_Basic_Arithmetic_Operations.java
+++ Q3AP_Beginner_Basic_Arithmetic_Operations.java
@@ -1,3 +1,33 @@
+// Problem Description
+
+// Print first five multiples of 3 that is :
+// 3 6 9 12 15
+
+
+// Problem Constraints
+
+// No constraints.
+
+
+// Input Format
+
+// No need to take input.
+
+
+// Output Format
+
+// Print 3 6 9 12 15 in single line, keep space between every multiple.
+
+
+// Example Input
+
+// No input reuired.
+
+
+// Example Output
+
+// Output :
+// 3 6 9 12 15
 import java.lang.*;
 import java.util.*;
 
