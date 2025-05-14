63eca2b944164845ca218e89be1f598033815809
iamxebakahn <zebadwight@gmail.com>
2025-05-14 09:09:03 +0530

    Add files via upload

---
+++ Q3AP_Beginner_Basic_Arithmetic_Operations.java
@@ -0,0 +1,19 @@
+import java.lang.*;
+import java.util.*;
+
+public class Q3AP_Beginner_Basic_Arithmetic_Operations{
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        for(int i=1;i<=5;i++)
+        {
+            System.out.print(i*3);
+                if(i!=5)
+                    {
+                        System.out.print(" ");
+                    }
+        }
+    }
+}
